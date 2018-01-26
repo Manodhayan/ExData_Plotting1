f997e606dbdc320b7868d8ca3a42acdcf8d1e743
Manodhayan <33039242+Manodhayan@users.noreply.github.com>
2018-01-27 00:59:52 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#Load power consumption data
+powcon <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(powcon) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+twodays <- subset(powcon,powcon$Date=="1/2/2007" | powcon$Date =="2/2/2007")
+
+#Basic plot function
+png("plot1.png", width=480, height=480)
+hist(as.numeric(as.character(twodays$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+# Graph Annotation
+title(main="Global Active Power")
+dev.off()
+
